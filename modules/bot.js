608e7150b2852ef62ad91976c36d7d6a9681b6a8
TommasoPadovano <79808184+TommasoPadovano@users.noreply.github.com>
2023-04-05 17:41:28 +0200

    bot class

---
+++ modules/bot.js
@@ -0,0 +1,8 @@
+class Bot {
+    constructor(name, brains, mouths, history) {
+        this.name = name;
+        this.brains = brains;
+        this.mouths = mouths;
+        this.history = history;
+    }
+}
