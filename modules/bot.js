41fd0a4862cb9aa0c445eb238bd7db85a2ff46da
Oussama Khaloui <mr.ucb06@gmail.com>
2023-04-11 17:33:06 +0200

    gitignore

--- modules/bot.js
+++ modules/bot.js
@@ -1,8 +1,29 @@
 class Bot {
-    constructor(name, brains, mouths, history) {
-        this.name = name;
-        this.brains = brains;
-        this.mouths = mouths;
-        this.history = history;
+    constructor(id, name, brains, mouths) {
+      this.id = id;
+      this.name = name;
+      this.state = state;
+      this.brains = brains;
+      this.mouths = mouths;
+      this.history = [];
     }
-}
+  
+    respond(input) {
+      // Process input using RiveScript
+      let output = this.brains.reply(this.id, input); // I'm not sure about this line...
+  
+      // Store conversation history
+      this.history.push({
+        input: input,
+        output: output,
+        timestamp: new Date(),
+      });
+  
+      // Return output using selected mouth
+      let selectedMouth = this.mouths[Math.floor(Math.random() * this.mouths.length)];
+      return selectedMouth(output);
+    }
+  }
+  
+  module.exports = Bot;
+  
---
+++ modules/botService.js
@@ -0,0 +1,14 @@
+const Bot = require('./bot');
+
+class BotService {
+    constructor() {
+        this.bots = [];
+    }
+
+    static async create(){
+        return new BotService();
+    }
+
+    static addBot()
+    }
+}
